9439fe5372bf2abd8cbd02cff441d2d9dea22e56
Caleb Creed <thirdcreed@gmail.com>
2013-04-11 07:51:35 -0700

    adds Kruskal's algorithm for minimum spanning tree

--- scripts/graphVis.js
+++ scripts/graphVis.js
@@ -21,10 +21,10 @@ function GraphViewModel() {
     self.marked = [];
     self.graph = ko.observableArray();
     self.$canvas = $('#nodesearch_canvas');
-    
+    self.connectionsMatrix = [];
 
     
-    
+
     
     self.graphTypes = [{
             graphType: "Directed"
@@ -33,7 +33,8 @@ function GraphViewModel() {
         }, {
             graphType: "Weighted"
         }];
-
+   
+    //SETUP
     self.bindNodeEvents = function(n, index, data) {
         var $n = $(n);
         var mousedownHandle = function(e, ui) {
@@ -77,10 +78,10 @@ function GraphViewModel() {
     self.addNode = function() {
         self.graph.push(new Node(self.nodeNum++, []));
     };
-    self.baseConnectionObject = function(e0, e1) {
+    self.baseConnectionObject = function(src, trgt) {
         return {
-            source: e0,
-            target: e1,
+            source: src,
+            target: trgt,
             connector: "StateMachine",
             paintStyle: {
                 lineWidth: 3,
@@ -102,7 +103,7 @@ function GraphViewModel() {
                 ]
             };
             jsPlumb.connect($.extend(obj, self.baseConnectionObject(e0, e1)));
-            self.graph()[e0.attr("id")].adj().push(e1.attr("id"));
+            self.graph()[e0.attr("id")].adj().push({id:e1.attr("id")});
         }
 
         if ($('#graphTypeId option:selected').text() == "Undirected") {
@@ -111,19 +112,16 @@ function GraphViewModel() {
             }, self.baseConnectionObject(e0, e1));
 
             jsPlumb.connect(c);
-            self.graph()[e0.attr("id")].adj().push(e1.attr("id"));
-            self.graph()[e1.attr("id")].adj().push(e0.attr("id"));
+            self.graph()[e0.attr("id")].adj().push({id:e1.attr("id")});
+            self.graph()[e1.attr("id")].adj().push({id:e0.attr("id")});
         }
         if ($('#graphTypeId option:selected').text() == "Weighted") {
             var weight = prompt("Enter a weight for this connection");
             var obj = {
-                endpoint: "Blank",
+       
+                endpoint: "Dot",
                 overlays: [
-                    ["PlainArrow", {
-                        location: 1,
-                        width: 20,
-                        length: 12
-                    }],
+                    
                     ["Label", {
                         label: "",
                         id: "label"
@@ -131,16 +129,34 @@ function GraphViewModel() {
                 ]
             };
             var c = $.extend(obj, self.baseConnectionObject(e0, e1));
-            c.getOverlay("label").setLabel("" + weight);
-            jsPlumb.connect(c);
+            var d = $.extend(obj, self.baseConnectionObject(e1, e0));
+            
+
+            var connection = jsPlumb.connect(c);
+               connection.getOverlay("label").setLabel("" + weight);
+            var connection2 = jsPlumb.connect(d);
+            
+            
+              
+
             self.graph()[e0.attr("id")].adj().push({
                 id: e1.attr("id"),
-                weight: weight
+                weight: weight,
+                gui: connection
             });
+            console.log(self.graph()[e0.attr("id")].adj())
+            self.graph()[e1.attr("id")].adj().push({
+                id: e0.attr("id"),
+                weight: weight,
+                gui:connection2
+
+            });
+            console.log(self.graph()[e1.attr("id")].adj())
  
         }
     };
 
+    //ALGORITHMS
     self.dfs = function(start, options) {
         numMarked = 0;
         stack = [];
@@ -156,7 +172,7 @@ function GraphViewModel() {
                 }, 1000);
                 adjList = self.graph()[currentNode].adj();
                 for (i = 0; i < adjList.length; i++) {
-                    stack.push(adjList[i]);
+                    stack.push(adjList[i].id);
                     console.log("STACK: ", stack);
                 }
             }
@@ -177,14 +193,94 @@ function GraphViewModel() {
                     opacity: 0.50
                 }, 1000);
                 adjList = self.graph()[currentNode].adj();
+                console.log(adjList);
                 for (i = 0; i < adjList.length; i++) {
-                    queue.unshift(adjList[i]);
+                    queue.unshift(adjList[i].id);
                     console.log("QUEUE: ", queue);
                 }
             }
         }
     };
 
+    self.kruskal = function () {
+        E = [];
+        p = [] //parentArray
+
+        for (var i = 0;i < self.graph().length; i++) {
+                  p[i] = null;
+        }
+
+        var find = function (  element ){
+            while ( p[element] != null) { 
+                console.log("element = p[element]:", element, " = ", p[element]);
+                element = p[element];
+               
+                
+            }
+            return element
+        }
+
+        var union = function (setA, setB) {
+           var setB = parseInt(setB)
+            console.log("Union of ", setA, " and ", setB);
+            var rootA = find(setA);
+            console.log("rootA: ", rootA );
+
+            var rootB = find(setB);
+            console.log("rootB: ", rootB);
+            if ( rootA == rootB ){
+                return
+            }
+            else {
+                p[rootA] = rootB;
+                console.log("Coloring: ", setA, setB);
+                
+                jsPlumb.select({ source: "" + setA, target: "" + setB }).setPaintStyle({ strokeStyle: "black", lineWidth: 20});
+               jsPlumb.select({ source: "" + setB, target: "" + setA }).setPaintStyle({ strokeStyle: "black", lineWidth: 20});
+               // if (p[setA] != null) self.graph()[setB].adj()[p[setA]].gui.getOverlay("label").setLabel("XXXXXX");
+            }
+        
+
+        }// end func
+
+        for (var i = 0; i < self.graph().length; i++) {
+            for(j = 0; j < self.graph()[i].adj().length; j++){
+                
+                E.push({from:i,to:self.graph()[i].adj()[j].id,w:self.graph()[i].adj()[j].weight});
+            }
+            
+        }
+        // sort E;
+        E.sort(function (a, b) {
+            return a.w - b.w
+        });
+        E.reverse();
+
+        var num = 0;
+        while (E.length > 0) {
+            var currentEdge = E.pop();
+            console.log(currentEdge);
+            union(currentEdge.from,currentEdge.to);
+            console.log("P during", num++,"th Iteration of while: ",p);
+           
+        }
+        console.log("P after while:", p);
+        for (var i = 0; i < p.length; i++) {
+            var blue = {
+                paintStyle: {
+                    lineWidth: 30,
+                    strokeStyle: "#056"
+                }
+            };
+            
+          
+
+       
+        }
+ 
+    }
+
+    //TEAR DOWN
     self.clearConnections = function() {
         jsPlumb.select().detach();
         self.marked = [];
